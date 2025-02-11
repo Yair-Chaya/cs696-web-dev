cd6d6cc0590585dc8e01e00ed83196bb3cffbd4e
Yair556 <yaircyair@gmail.com>
2025-02-11 18:44:48 -0500

    Add user greeting routes to Flask application

--- myFirstFlaskApp/app.py
+++ myFirstFlaskApp/app.py
@@ -10,8 +10,18 @@ app = Flask(__name__)
 def hello_world():
     # This function is called when the root URL is accessed. It returns the string
     # "Hello, World!" which is displayed in the client's web browser.
-    return '', 418
+    return 'Hello, World', 200
 
+@app.route('/users/<username>')
+def hello_username(username):
+    # This function is called when a URL matching "/users/<username>" is accessed.
+    return f'Hello, {username}', 200
+
+@app.route('/users/<username>/<age>')
+def hello_username_Age(username, age):
+    # This function is called when a URL matching "/users/<username>" is accessed.
+    return f'Hello, {username}. You are {age} years old', 200
+ 
 # Check if the executed script is the main program and run the Flask application.
 # The debug=True option enables Flask's debugger, showing detailed error pages when an error occurs.
 if __name__ == '__main__':
