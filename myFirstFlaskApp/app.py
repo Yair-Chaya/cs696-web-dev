b3512ed59c0aa9dabf8eeafaaed0653cf4861311
Yair556 <yaircyair@gmail.com>
2025-02-11 19:16:05 -0500

    Implement template rendering for user greetings and add base HTML structure

--- myFirstFlaskApp/app.py
+++ myFirstFlaskApp/app.py
@@ -1,5 +1,6 @@
 # Import the Flask class from the flask module
 from flask import Flask
+from flask import render_template
 
 # Create an instance of the Flask class. This instance will be our WSGI application.
 app = Flask(__name__)
@@ -10,17 +11,17 @@ app = Flask(__name__)
 def hello_world():
     # This function is called when the root URL is accessed. It returns the string
     # "Hello, World!" which is displayed in the client's web browser.
-    return 'Hello, World', 200
+    return render_template('index.html')
 
 @app.route('/users/<username>')
 def hello_username(username):
     # This function is called when a URL matching "/users/<username>" is accessed.
-    return f'Hello, {username}', 200
+    return render_template('hello_name.html', name=username)
 
 @app.route('/users/<username>/<age>')
 def hello_username_Age(username, age):
     # This function is called when a URL matching "/users/<username>" is accessed.
-    return f'Hello, {username}. You are {age} years old', 200
+    return render_template('hello_name_age.html', name=username, age=age)
  
 # Check if the executed script is the main program and run the Flask application.
 # The debug=True option enables Flask's debugger, showing detailed error pages when an error occurs.
